1cd77a71d4c05489b891355f2d5bc44d7b84b4b6
Sowmya R <sowmya.tech17@gmail.com>
2026-08-16 00:18:17 +0530

    Add files via upload

---
+++ dll.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        self.prev=None
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head=None
+
+    def insertathead(self,x):
+        ni=Node(x)
+        if(self.head==None):
+            self.head=ni
+            return
+        curr=self.head
+        curr.prev=ni
+        ni.next=curr
+        self.head=ni
+        
