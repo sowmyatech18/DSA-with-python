9546f972cbce05d0de7e0bd1667e73f26943c25d
Sowmya R <sowmya.tech17@gmail.com>
2026-08-16 01:27:39 +0530

    Enhance DoubleLinkedList with additional methods
    Added methods to delete from tail and insert at specific positions in the double linked list.

--- dll.py
+++ dll.py
@@ -39,4 +39,63 @@ class DoubleLinkedList:
         self.head=curr.next
         curr.next.prev=None
         curr=None
+    def deleteattail(self):
+        #empty list
+        if(self.head==None):
+            return
+        #one element list
+        if(self.head.next==None):
+            self.head=None
+            return
+        curr=self.head
+        while(curr.next.next):
+            curr=curr.next
+        curr.next=None
+    def displayall(self):
+        curr=self.head
+        if(curr==None):
+            return
+        while(curr):
+            print(curr.data)
+            curr=curr.next
+    def insertatk(self,k,x):
+        ni=Node(x);
+        curr=self.head
+        count=0
+        while(curr):
+            count+=1
+            curr=curr.next
+        if(k==0):
+            self.insertathead(x)
+        if(k==(count-1)):
+            self.insertattail(x)
+        if(k<0 or k>count):
+            print("Out of range")
+        curr=self.head
+        for i in range(k):
+            curr=curr.next
+        ni.prev=curr.prev
+        curr.prev.next=ni
+        ni.next=curr
+        curr.prev=ni
+    def insertatkback(self,k,x):
+            ni=Node(x);
+            curr=self.head
+            count=0
+            while(curr):
+                count+=1
+                curr=curr.next
+            if(k==0):
+                self.insertathead(self,x)
+            if(k==(count-1)):
+                self.insertattail(self,x)
+            if(k<0 or k>count):
+                print("Out of range")
+            curr=self.head
+            for i in range(count-k-1):
+                curr=curr.next
+            ni.prev=curr
+            ni.next=curr.next
+            curr.next=ni
+            ni.next.prev=ni
         
