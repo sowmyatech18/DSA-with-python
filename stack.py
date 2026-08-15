bd331c122dab8f1770607207783fb103419609cd
Sowmya R <sowmya.tech17@gmail.com>
2026-08-16 01:58:32 +0530

    Refactor stackArray and fix typo in peek method
    Fixed a typo in the peek method's error message and changed the third push value from 7 to 10.

--- stack.py
+++ stack.py
@@ -3,35 +3,29 @@ class stackArray:
         self.arr=[0]*cap
         self.cap=cap
         self.top=-1
-
     def push(self,x):
         if(self.top==(self.cap-1)):
             print("stack overflow")
             return
         self.top+=1
         self.arr[self.top]=x
-        
-
     def pop(self):
         if(self.top==-1):
             print("stack underflow")
-            return 
+            return
         v=self.arr[self.top]
         self.top-=1
         return v
-
     def peek(self):
         if(self.top==-1):
-            print("empty stack")
+            print("empty satck")
         v=self.arr[self.top]
         return v
-
 s=stackArray(4)
 s.push(3)
 s.push(8)
+s.push(10)
 s.push(1)
-s.push(7)
 print(s.pop())
 print(s.peek())
 print(s.arr)
-
