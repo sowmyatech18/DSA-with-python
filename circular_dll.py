172e143b0e025b833ef71fd7c3fecef6db42a676
Sowmya R <sowmya.tech17@gmail.com>
2026-08-16 11:03:18 +0530

    insert and delete methods
    Added methods to insert at tail and delete from head/tail in circular doubly linked list.

--- circular_dll.py
+++ circular_dll.py
@@ -21,6 +21,51 @@ class circular_dll:
     ni.next=self.head
     self.head.prev=ni
     self.head=ni
+
+  def insertattail(self,x):
+      ni=Node(x)
+      if(self.head==None):
+        self.head=ni
+        ni.next=self.head
+        ni.prev=self.head
+        return
+      curr=self.head
+      while(curr.next!=self.head):
+        curr=curr.next
+      curr.next=ni
+      ni.prev=curr
+      ni.next=self.head
+      self.head.prev=ni
+  
+  def deleteathead(self):
+    if(self.head==None):
+      return
+    if(self.head.next==self.head):
+      self.head=None
+      return
+    curr=self.head
+    while(curr.next!=self.head):
+      curr=curr.next
+    curr.next=self.head.next
+    self.head=self.head.next
+    self.head.prev=curr
+  
+  def deleteattail(self):
+    if(self.head==None):
+      return
+    if(self.head.next==self.head):
+      self.head=None
+      return
+    curr=self.head
+    while(curr.next.next!=self.head):
+      curr=curr.next
+    curr.next=self.head
+    self.head.prev=curr
+    
+  
+    
+    
+    
     
       
       
