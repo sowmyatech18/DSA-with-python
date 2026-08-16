19666afa1978a7751ecd1497dcc5b78022ffdec7
Sowmya R <sowmya.tech17@gmail.com>
2026-08-16 10:41:13 +0530

    Refactor circular linked list implementation

--- circular_sll.py
+++ circular_sll.py
@@ -3,41 +3,54 @@ class Node:
         self.data=data
         self.next=None
 
+class circular_sll:
+    def __init__(self):
+        self.head=None
+        
     def insertathead(self,x):
         ni=Node(x)
         if(self.head==None):
             self.head=ni
+            ni.next=self.head
             return
         curr=self.head
         ni.next=curr
         self.head=ni
+        while(curr.next!=self.head):
+            curr=curr.next
+        curr.next=self.head
+        
     def insertattail(self,x):
         ni=Node(x)
         if(self.head==None):
             self.head=ni
+            ni.next=self.head
             return
         curr=self.head
-        while(curr.next):
+        while(curr.next!=self.head):
             curr=curr.next
         curr.next=ni
         ni.next=self.head
     def deleteathead(self):
         if(self.head==None):
             return
-        if(self.head.next==None):
+        if(self.head.next==self.head):
             self.head=None
             return
         curr=self.head
-        self.head=curr.next
-        curr=None
+        while(curr.next!=self.head):
+            curr=curr.next
+        self.head=self.head.next
+        curr.next=self.head
     def deleteattail(self):
         if(self.head==None):
             return
-        if(self.head.next==None):
+        if(self.head.next==self.head):
             self.head=None
             return
         curr=self.head
-        while(curr.next.next):
+        while(curr.next.next!=self.head):
             curr=curr.next
-        curr.next=None
+        curr.next=self.head
+
     
