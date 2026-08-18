d315a331793f4ef9e95b3094078f88a217879e91
Sowmya R <sowmya.tech17@gmail.com>
2026-08-18 21:28:56 +0530

    Update queue_sll.py

--- queue_sll.py
+++ queue_sll.py
@@ -1,9 +1,12 @@
 class Node:
     def __init__(self,data):
-        self.next=None
         self.data=data
+        self.next=None
 
 class queueLinkedList:
     def __init__(self):
         self.front=None
         self.rear=None
+
+    def enque
+        
