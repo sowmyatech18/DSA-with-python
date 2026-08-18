8a8794d980bdda6f0d7a5c721f5e3c60c955b7ea
Sowmya R <sowmya.tech17@gmail.com>
2026-08-18 22:13:55 +0530

    Add queue operations and example usage to queue_sll.py

--- queue_sll.py
+++ queue_sll.py
@@ -8,5 +8,45 @@ class queueLinkedList:
         self.front=None
         self.rear=None
 
-    def enque
+    def enqueue(self,x):
+        ni=Node(x)
+        if(self.rear==None):
+            self.front=ni
+            self.rear=ni
+            return
+        self.rear.next=ni
+        self.rear=ni
+    def dequeue(self):
+        if(self.front==None):
+            print("queue underflow")
+            return
+        v=self.front.data
+        self.front=self.front.next
+        if(self.front==None):
+            self.rear=None
+        return v
+    def peek(self):
+        if(self.front==None):
+            print("queue underflow")
+            return
+        v=self.front.data
+        return v
+    def display(self):
+        if(self.front==None):
+            print("queue underflow")
+            return
+        curr=self.front
+        while(curr):
+            print(curr.data,end=" ")
+            curr=curr.next
+s=queueLinkedList()
+s.enqueue(3)
+s.enqueue(8)
+s.enqueue(10)
+s.enqueue(1)
+print(s.dequeue())
+print(s.peek())
+s.display()
+        
+        
         
