fadf761ccba653601723c6e52a761fbe8d4f42e8
Sowmya R <sowmya.tech17@gmail.com>
2026-08-18 21:27:06 +0530

    Implement basic queue using singly linked list

---
+++ queue_sll.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self,data):
+        self.next=None
+        self.data=data
+
+class queueLinkedList:
+    def __init__(self):
+        self.front=None
+        self.rear=None
