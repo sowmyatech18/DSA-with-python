bff787b792c5cfc25ff192bf8b21e26fb181cdaf
Sowmya R <sowmya.tech17@gmail.com>
2026-08-18 21:12:59 +0530

    Convert queueArray to class with additional methods
    Refactor queueArray to a class and add dequeue and peek methods.

--- queue_array.py
+++ queue_array.py
@@ -1,4 +1,4 @@
-def queueArray:
+class queueArray:
 def __init__(self,cap):
   self.arr=[0]*cap
   self.cap=cap
@@ -11,3 +11,21 @@ def __init__(self,cap):
       return
     self.rear+=1
     self.arr[self.rear]=x
+
+  def dequeue(self):
+    if(self.front>self.rear):
+      print("queue underflow")
+      return
+    v=self.arr[self.front]
+    self.front+=1
+    return v
+
+  def peek(self):
+    if(self.front>self.rear):
+      print("queue empty")
+      return
+    v=self.arr[self.front]
+    return v
+
+
+    
